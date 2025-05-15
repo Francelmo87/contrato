07ac8e7ff2b77fadea548f7cf006e6b7e8f5b8b1
Francelmo <francelmo87@hotmail.com>
2025-05-15 09:10:05 -0300

    created the model base

---
+++ app/base/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(
+        'criado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+    modified_at = models.DateTimeField(
+        'modificado em',
+        auto_now_add=False,
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
