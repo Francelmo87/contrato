0136bc0b1bd5056e5b25ed3a042153ad5d5bb2cd
Francelmo <francelmo87@hotmail.com>
2025-05-15 09:12:20 -0300

    created the app supplies

---
+++ app/suppliers/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from app.base.models import TimeStampedModel
+
+# Fornecedor
+class Supplier(TimeStampedModel):
+    name = models.CharField('empresa', max_length = 200, null=True, blank=True)
+    cnpj = models.CharField('cnpj', max_length = 17, null=True, blank=True)
+    representative = models.CharField('representante', max_length = 200, null=True, blank=True)
+    rg = models.CharField('rg', max_length = 13, null=True, blank=True)
+    cpf = models.CharField('cpf', max_length = 11, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
