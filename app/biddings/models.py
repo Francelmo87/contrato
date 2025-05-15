29eac8861e28f8cc51b9bfa6e2a768a9605534ee
Francelmo <francelmo87@hotmail.com>
2025-05-15 09:09:27 -0300

    created the app bidding

---
+++ app/biddings/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from app.base.models import TimeStampedModel
+
+# Licitação
+class Bidding(TimeStampedModel):
+    number = models.CharField('numero', max_length=50)
+    modality = models.CharField('modalidade', max_length=100)
+    year = models.PositiveIntegerField('ano')
+    approval_date = models.DateField('data da Homologação')
+    value = models.DecimalField('valor', max_digits=11, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        ordering = ['number']
+    
+    def __str__(self):
+        return f"{self.modality} {self.number}/{self.year}"
