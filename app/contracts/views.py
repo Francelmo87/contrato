afebfa86d2562fa7c379116f2f0ad85f799eae8c
Francelmo <francelmo87@hotmail.com>
2025-05-20 10:06:36 -0300

    created the view of contracts

--- app/contracts/views.py
+++ app/contracts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from . models import Contract
+
+class ContractListView(ListView):
+    model= Contract
+    template_name = 'contract_list.html'
+    context_object_name = 'contracts'
