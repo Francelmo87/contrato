fa6946d5e9460395dddec3a8a77004c16064206e
Francelmo <francelmo87@hotmail.com>
2025-05-21 08:44:00 -0300

    created the search in view

--- app/contracts/views.py
+++ app/contracts/views.py
@@ -7,3 +7,10 @@ class ContractListView(ListView):
     model= Contract
     template_name = 'contract_list.html'
     context_object_name = 'contracts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(name__icontains=search)
+        return queryset
