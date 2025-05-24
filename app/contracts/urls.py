58f36b93d2e41a804506bf6c6e40b5fa1c62bfcb
Francelmo <francelmo87@hotmail.com>
2025-05-24 17:39:09 -0300

    created the router of contract_create

--- app/contracts/urls.py
+++ app/contracts/urls.py
@@ -6,5 +6,6 @@ app_name='contracts'
 
 urlpatterns = [
     path('list/', v.ContractListView.as_view(), name='contract_list' ),
+    path('create/', v.ContractCreateView.as_view(), name='contract_create' ),
     path('<int:pk>/detail/', v.ContractDetailView.as_view(), name='contract_detail' ),
 ]
