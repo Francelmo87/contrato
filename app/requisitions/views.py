13ac36bed730b29e770c543d6ff80245af739c37
Francelmo <francelmo87@hotmail.com>
2025-05-25 21:26:31 -0300

    created the view of requisitions

--- app/requisitions/views.py
+++ app/requisitions/views.py
@@ -1,4 +1,11 @@
+from django.views.generic import ListView
 
 
+from .models import Requisition
 
+
+class RequisitionListView(ListView):
+    model = Requisition
+    template_name = 'requisition_list.html'
+    context_object_name = 'requisitions'
 
