445b6e533f204e99ecb6e660db903e7b2691d359
Francelmo <francelmo87@hotmail.com>
2025-05-28 09:28:21 -0300

    created the router of movements's list

--- app/dispatches/urls.py
+++ app/dispatches/urls.py
@@ -6,4 +6,6 @@ app_name = 'dispatches'
 urlpatterns = [
     path('pending/', v.PendingRequisitionListView.as_view(), name='pending_list'),
     path('<int:pk>/approve/', v.RequisitionApprovalView.as_view(), name='approve_requisition'),
+
+    path('movement_list/', v.MovimentListView.as_view(), name='moviment_list'),
 ]
