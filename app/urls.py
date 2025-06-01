6ebc21be9f31b4ed9fa6fb935d166fe2277c9531
Francelmo <francelmo87@hotmail.com>
2025-06-01 09:47:43 -0300

    created the logout of system

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login/', LoginView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(), name = 'logout'),
 
     path('', include('app.base.urls')),
     path('contracts/', include('app.contracts.urls')),    
